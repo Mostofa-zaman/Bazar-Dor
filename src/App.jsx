3665656fc98f18d1b2c2b2637acfa746fcf6c34f
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-02 23:46:47 +0600

    update Container component with dynamic className support

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Button from "./components/common/Button";
+import Container from "./components/common/Container";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world
+    <div className=''>
+      <Container className='bg-amber-600 h-[200px]'>
+<Button title="Click me" color="bg-success" />
+      </Container>
         
-      </h1>
+      
     </div>
   );
 }
--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-
-const Container = ({ children }) => {
-  return <div className="max-w-[1320px] mx-auto px-5 lg:px-0">{children}</div>;
+const Container = ({ children, className }) => {
+  return (
+    <div
+      className={`max-w-[1320px] mx-auto px-5 lg:px-0 ${className}`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;

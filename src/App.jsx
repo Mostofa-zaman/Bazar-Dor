09882a795e9aa938c1b1464019f01952cb158a2f
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-03 00:28:19 +0600

    feat(ui): build responsive Footer with newsletter and app download section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Footer from "./components/common/Footer";
 import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-  
+  <Footer/>
     </div>
   );
 }

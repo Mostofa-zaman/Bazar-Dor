fe54caaba6acd07a00b0e502e60b7f718cabddbc
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-04 00:53:06 +0600

    add HighPriceSlider section with CategoryHighPrices Swiper integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BannerSliders from '../components/home/bannerSlider/Index'
 import Category from '../components/home/category/Index'
 import HomeProducts from '../components/home/homeProducts'
+import HighPriceSlider from '../components/home/highPriceSlider/Index'
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
         <BannerSliders/>
         <Category/> 
         <HomeProducts/>
+        <HighPriceSlider/>
         
     </div>
   )

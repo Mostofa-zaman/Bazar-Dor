331b1664db54a95562b2b16dbc6b92eff52af7fd
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-04 00:06:01 +0600

    feat: add CategoryUIOne component with dynamic background color

---
+++ src/components/common/CategroyUiOne.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const CategroyUIOne = ({  description, name, imgSrc, imgAlt ,bgColor}) => {
+  return (
+    <div
+      className={`p-5 min-h-[290px]  rounded-[4px] `}
+      style={{ backgroundColor: bgColor }}
+    >
+      <figure>
+        <img
+          src={imgSrc}
+          alt={imgAlt}
+          className=" object-cover  w-full"
+          style={{height:"150px"}}
+        />
+      </figure>
+      <div className="pt-[18px] space-y-1">
+        <p className="navitems text-center text-primary">{name}</p>
+        <p className="para-normal text-center text-primary">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategroyUIOne;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import BannerSliders from '../components/home/bannerSlider/Index'
+import Category from '../components/home/category/Index'
 
 const Home = () => {
   return (
     <div>
 
         <BannerSliders/>
+        <Category/>
     </div>
   )
 }

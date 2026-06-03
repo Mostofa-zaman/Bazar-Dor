fc10ca54e72d50d985c03d4def42a83f5275a965
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-03 23:48:28 +0600

    add BannerSliders component with Swiper autoplay integration

--- src/components/home/bannerSlider/Index.jsx
+++ src/components/home/bannerSlider/Index.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
-const Index = () => {
+import SliderCard from "../../common/SliderCard";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Autoplay } from "swiper/modules";
+import { productsBanner } from "../../../helper/projectArrayObj";
+
+const BannerSliders = () => {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
   return (
-    <div>Index</div>
-  )
-}
+    <div>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 4500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        {productsBanner.map((items, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <SliderCard product={items} />
+            </SwiperSlide>
+          );
+        })}
 
-export default Index
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BannerSliders;

a274a8307f7b3daec630bbdec0ee1081bdc74ede
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-08 23:30:09 +0600

    feat: add market price page and route setup

---
+++ src/pages/MarketPrice.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bredcrumb from '../components/common/Bredcrumb'
+
+const MarketPrice = () => {
+  return (
+   <>
+   {/* <Bredcrumb/> */}
+rjigheife
+   </>
+  )
+}
+
+export default MarketPrice

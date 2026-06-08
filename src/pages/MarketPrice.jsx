4dacb49a9f0747ec269aec780d454f464755853e
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-08 23:39:03 +0600

    feat: add product filter panel with category and sorting options

--- src/pages/MarketPrice.jsx
+++ src/pages/MarketPrice.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Bredcrumb from '../components/common/Bredcrumb'
 import MarketLocationForm from "../components/marketPrice/marketLocationForm/Index";
+import FilterPanel from '../components/marketPrice/filterPanel/Index';
 const MarketPrice = () => {
   return (
    <>
    <Bredcrumb/>
   <MarketLocationFrom/>
+  <FilterPanel/>
   </>
   )
 }

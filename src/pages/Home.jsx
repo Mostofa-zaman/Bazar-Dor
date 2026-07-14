12054d3726ff5ef3193b00348d080d16458c86f2
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-14 23:30:46 +0600

    check problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import LowPriceSlider from '../components/home/lowPriceSlider/Index'
 import GetTheApp from '../components/home/getTheApp/Index'
 import GovtMess from '../components/home/govtMessage/Index'
 import PastWeekMarket from '../components/home/pastWeekMarket/Index'
-import HomeProducts from '../components/home/homeProducts'
+import HomeProducts from '../components/home/homeProducts/Index'
 
 
 

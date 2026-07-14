a6fe78701432c49af71b1bd3e264167176a5b309
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-14 22:12:20 +0600

    fix: rename index.jsx to Index.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import BannerSliders from '../components/home/bannerSlider/Index'
 import Category from '../components/home/category/Index'
-import HomeProducts from '../components/home/homeProducts/Index'
 import HighPriceSlider from '../components/home/highPriceSlider/Index'
 import LowPriceSlider from '../components/home/lowPriceSlider/Index'
 import GetTheApp from '../components/home/getTheApp/Index'
 import GovtMess from '../components/home/govtMessage/Index'
 import PastWeekMarket from '../components/home/pastWeekMarket/Index'
+import HomeProducts from '../components/home/homeproducts/Index'
 
 
 const Home = () => {
